ae4a1e36f285ac678dd109fcb66b868aa67af69a
dhifafaz <dhifaf.az@gmail.com>
2021-01-21 12:15:35 +0700

    Go

---
+++ GO-2-Variables.go
@@ -0,0 +1,20 @@
+//Variables
+/*For Multi Line
+comments*/
+package main
+
+import "fmt"
+
+func main() {
+	//	Static type declaration
+	var num int
+	num = 10
+	var name string = "dhifaf"
+	var PI float64 = 3.14
+	fmt.Println("Number", num, "\nName", name, "\nPI", PI)
+	//Dynamic Tyed Variable
+	var number, name1, Pi = 10, "Dhifaf", 3.14
+	fmt.Println(number, name1, Pi)
+	numb := 100
+	fmt.Println(numb)
+}
